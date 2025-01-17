d9baa5fae6ed7486f96f777c43b6cbce3d940974
Lucke2006 <jigmaya@gmail.com>
2025-01-17 13:59:19 -0400

    simple collisions

---
+++ cmake-build-debug/_deps/flac-build/src/libFLAC/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/nesto/Dropbox/Trabajos Jose Ignacio/UniMary/Semester 4/CSC204/SFML-CSC-NEW/cmake-build-debug/_deps/flac-src/src/libFLAC
+# Build directory: C:/Users/nesto/Dropbox/Trabajos Jose Ignacio/UniMary/Semester 4/CSC204/SFML-CSC-NEW/cmake-build-debug/_deps/flac-build/src/libFLAC
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,42 @@
 #include <SFML/Graphics.hpp>
+#define SCREENW 2560u
+#define SCREENH 1600u
+
+class circle{
+public:
+    int x = 0;
+    int y = 0;
+    int vx = 1;
+    int vy = 1;
+    int r = 100;
+    auto drawCircle(){
+        sf::CircleShape shape(100);
+        shape.setFillColor(sf::Color(100,250,50));
+        shape.setPosition(sf::Vector2f(x,y));
+        return shape;
+    }
+};
+
+void collisions(circle *c){
+    if(c->y + c->r >= SCREENH){
+        c->vy *= -1;
+    }
+    if(c->y < 0u){
+        c->vy *= -1;
+    }
+//    if(c->x + c->r >= SCREENW){
+//        c->vx *= -1;
+//    }
+//    if(c->x < 0u){
+//        c->vx *= -1;
+//    }
+}
 
 int main()
 {
-    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
+    circle c;
+
+    auto window = sf::RenderWindow(sf::VideoMode({SCREENW, SCREENH}), "CMake SFML Project");
     window.setFramerateLimit(144);
 
     while (window.isOpen())
@@ -15,7 +49,17 @@ int main()
             }
         }
 
+
+
         window.clear();
+//        sf::CircleShape shape(100);
+//        shape.setFillColor(sf::Color(100,250,50));
+//        shape.setPosition(sf::Vector2f(1000,1000));
+        window.draw(c.drawCircle());
+        c.x = c.x + 2*c.vx;
+        c.y = c.y + 2*c.vy;
+        collisions(&c);
+
         window.display();
     }
 }
